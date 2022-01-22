db843bdb506fe2158237825be3acbb59ced12381
Pavel Zagrebelin <pavel@zagrebelin.ru>
2022-01-23 04:27:07 +0500

    day 5

--- src/day_05/mod.rs
+++ src/day_05/mod.rs
@@ -2,9 +2,6 @@
 // sample B = 12
 // real A = 6841
 // real B = 19258
-extern crate regex;
-
-use regex::{Captures, Regex};
 use std::cmp::{max, min, Ordering};
 use std::collections::HashMap;
 
@@ -18,24 +15,44 @@ use std::thread::yield_now;
 
 const FILENAME: &str = "inputs/05.txt";
 
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl FromStr for Point {
+    type Err = std::num::ParseIntError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let p: Vec<&str> = s.split(",").collect();
+        Ok(Point {
+            x: p[0].parse().unwrap(),
+            y: p[1].parse().unwrap(),
+        })
+    }
+}
+impl Point {
+    fn left_than(&self, other: &Point) -> bool {
+        self.x < other.x
+    }
+
+    fn above_than(&self, other: &Point) -> bool {
+        self.y < other.y
+    }
+}
 struct Line {
-    x1: i32,
-    y1: i32,
-    x2: i32,
-    y2: i32,
+    a: Point,
+    b: Point,
 }
 
 impl FromStr for Line {
     type Err = std::num::ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let p1: Vec<&str> = s.split(" -> ").flat_map(|p| p.split(",")).collect();
+        let p1: Vec<&str> = s.split(" -> ").collect();
 
         Ok(Line {
-            x1: p1[0].parse().unwrap(),
-            y1: p1[1].parse().unwrap(),
-            x2: p1[2].parse().unwrap(),
-            y2: p1[3].parse().unwrap(),
+            a: Point::from_str(p1[0]).unwrap(),
+            b: Point::from_str(p1[1]).unwrap(),
         })
     }
 }
@@ -43,73 +60,60 @@ impl FromStr for Line {
 impl Line {
     fn points(&self, with_diag: bool) -> Vec<[i32; 2]> {
         let mut points: Vec<[i32; 2]> = Vec::new();
-        if self.x1 == self.x2 {
-            let miny = min(self.y1, self.y2);
-            let maxy = max(self.y1, self.y2);
-            for y in miny..maxy + 1 {
-                points.push([self.x1, y])
+        if self.a.x == self.b.x {
+            // особый случай - вертикальная палка
+            let top = match self.a.above_than(&self.b) {
+                true => &self.a,
+                false => &self.b,
+            };
+
+            for y in 0..self.length() + 1 {
+                points.push([self.a.x, top.y + y])
             }
-        } else if self.y1 == self.y2 {
-            let minx = min(self.x1, self.x2);
-            let maxx = max(self.x1, self.x2);
-            for x in minx..maxx + 1 {
-                points.push([x, self.y1])
-            }
-        } else if with_diag {
-            let mut left_x = 0;
-            let mut left_y = 0;
-            let mut slope = 0;
-            if self.x1 < self.x2 {
-                left_x = self.x1;
-                left_y = self.y1;
-                slope = match self.y1.cmp(&self.y2) {
-                    Ordering::Less => 1,
-                    Ordering::Equal => 0,
-                    Ordering::Greater => -1,
-                }
+        } else {
+            // горизонтальная или наклонная линия
+            let mut left: &Point;
+            let mut right: &Point;
+            if self.a.left_than(&self.b) {
+                left = &self.a;
+                right = &self.b;
             } else {
-                left_x = self.x2;
-                left_y = self.y2;
-                slope = match self.y2.cmp(&self.y1) {
-                    Ordering::Less => 1,
-                    Ordering::Equal => 0,
-                    Ordering::Greater => -1,
-                }
+                left = &self.b;
+                right = &self.a;
             }
-            let length = (self.x1 - self.x2).abs();
-            for i in 0..length + 1 {
-                points.push([left_x + i, left_y + slope * i]);
+            let slope = match left.y.cmp(&right.y) {
+                Ordering::Less => 1,
+                Ordering::Equal => 0,
+                Ordering::Greater => -1,
+            };
+            if with_diag || slope == 0 {
+                for i in 0..self.length() + 1 {
+                    points.push([left.x + i, left.y + slope * i]);
+                }
             }
         }
 
         points
     }
+
+    fn length(&self) -> i32 {
+        return max((self.a.x - self.b.x).abs(), (self.a.y - self.b.y).abs());
+    }
 }
 
 pub fn solve_a() -> i32 {
-    let lines = read_data();
-    let mut field: HashMap<[i32; 2], i32> = HashMap::new();
-    for line in lines.iter() {
-        for point in line.points(false) {
-            if !field.contains_key(&point) {
-                field.insert(point, 0);
-            }
-            let old = field.get(&point).unwrap();
-            field.insert(point, old + 1);
-        }
-    }
-    let counter = field.values().into_iter().filter(|v| **v >= 2).count();
-
-    counter as i32
+    solve(false)
 }
 
 pub fn solve_b() -> i32 {
-    println!("Before read");
+    solve(true)
+}
+
+fn solve(with_diag: bool) -> i32 {
     let lines = read_data();
-    println!("After read");
     let mut field: HashMap<[i32; 2], i32> = HashMap::new();
     for line in lines.iter() {
-        for point in line.points(true) {
+        for point in line.points(with_diag) {
             if !field.contains_key(&point) {
                 field.insert(point, 0);
             }
@@ -117,11 +121,10 @@ pub fn solve_b() -> i32 {
             field.insert(point, old + 1);
         }
     }
-    println!("Count");
     let counter = field.values().into_iter().filter(|v| **v >= 2).count();
-    println!("Done");
     counter as i32
 }
+
 fn read_data() -> Vec<Line> {
     let mut lines = common::read_lines2(FILENAME);
     let steps: Vec<Line> = lines
