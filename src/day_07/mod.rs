dea83279ccf977df563cf78649fba76d16b4bcd6
Pavel Zagrebelin <pavel@zagrebelin.ru>
2022-01-23 15:52:51 +0500

    day 7 refactor

--- src/day_07/mod.rs
+++ src/day_07/mod.rs
@@ -1,7 +1,7 @@
 // sample A = 37
 // sample B = 168
 // real A = 340052
-// real B =
+// real B = 92948968
 
 use crate::common;
 use std::collections::HashMap;
@@ -10,25 +10,24 @@ use std::fs::read;
 const FILENAME: &str = "inputs/07.txt";
 
 pub fn solve_a() -> i32 {
-    let crabs = read_data();
-    let begin = *crabs.iter().min().unwrap();
-    let end = *crabs.iter().max().unwrap();
-    let mut ans: HashMap<i32, i32> = HashMap::new();
-    for pos in begin..end + 1 {
-        ans.insert(pos, crabs.iter().map(|c| distance_a(pos, *c)).sum());
-    }
-    ans.iter().map(|(key, value)| *value).min().unwrap()
+    solve(distance_a, "outputs/07_a.png")
 }
 
 pub fn solve_b() -> i32 {
+    solve(distance_b, "outputs/07_b.png")
+}
+
+pub fn solve(fit_function: fn(i32, i32) -> i32, fname: &str) -> i32 {
     let crabs = read_data();
     let begin = *crabs.iter().min().unwrap();
     let end = *crabs.iter().max().unwrap();
     let mut ans: HashMap<i32, i32> = HashMap::new();
     for pos in begin..end + 1 {
-        ans.insert(pos, crabs.iter().map(|c| distance_b(pos, *c)).sum());
+        ans.insert(pos, crabs.iter().map(|c| fit_function(pos, *c)).sum());
     }
-    ans.iter().map(|(key, value)| *value).min().unwrap()
+    draw_chart(&crabs, &ans, fname);
+
+    ans.iter().map(|(key, value)| *value).min().unwrap();
 }
 
 fn distance_a(a: i32, b: i32) -> i32 {
